4f6043045b4bd82790982f9f624f473058e7932e
kat7058 <katerinag17019@gmail.com>
2023-03-14 16:28:59 -0600

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
  
 
   return (
     <div>
-   
+   <Navbar/>
     </div>
   )
 }
