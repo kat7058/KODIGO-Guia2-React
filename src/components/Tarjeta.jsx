030ddcc3c94ea0cdf1480063b74a6e49050c138d
mauricio-esquivel <122753981+mauricio-esquivel@users.noreply.github.com>
2023-03-14 21:24:32 -0600

    nuevos cambios en tarjeta

--- src/components/Tarjeta.jsx
+++ src/components/Tarjeta.jsx
@@ -2,15 +2,20 @@ import React, { useState, useEffect } from "react"
 
 export default function Tarjeta(props){
     const [rickandmorty, setRickandmorty]= useState({});
+    /**estado para manejo de location */
+    const [location, setLocation] = useState ({});
 
     const obtenerRickandMortyByUrl= async (url) =>{
         let respuesta = await fetch(url);
         let datos = await respuesta.json();
 
         setRickandmorty(datos);
+        setLocation(datos.location);
     }
+
+
     useEffect(() => {
-obtenerRickandMortyByUrl(props.url);
+    obtenerRickandMortyByUrl(props.url);
 
     },[]);
     console.log(rickandmorty);
@@ -41,7 +46,12 @@ obtenerRickandMortyByUrl(props.url);
                     <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div className="modal-body">
-                    ...
+                    <p><b>ID: {rickandmorty.id}</b></p>
+                    <p><b>Status: {rickandmorty.status}</b></p>
+                    <p><b>Species: {rickandmorty.species}</b></p>
+                    <p><b>Gender: {rickandmorty.gender}</b></p>
+                    <p><b>Location: {location.name}</b></p>
+
                 </div>
                 <div className="modal-footer">
                     <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
