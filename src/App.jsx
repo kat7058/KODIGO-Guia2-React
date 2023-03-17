5286bde6c655a78810858660e25760e716740418
mauricio-esquivel <122753981+mauricio-esquivel@users.noreply.github.com>
2023-03-16 23:29:00 -0600

    actualizando la app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
 import ListaRickandMorty from './components/ListaRickandMorty'
+import imagen from './assets/rick-and-morty-top-banner.png'
 
 function App() {
  
 
   return (
     <div>
-          <Navbar name="Jovenes"/>
+      <header>
+        <img src={imagen} className="img-fluid" alt=""/>
+      </header>
+          
+      <Navbar name="Jovenes"/>
     </div>
   )
 }
