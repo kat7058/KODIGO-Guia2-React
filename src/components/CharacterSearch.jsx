7571aff9a72aa7706f2612cb440f089c37d283ef
kat7058 <katerinag17019@gmail.com>
2023-04-10 23:02:42 -0600

    Finalizacion de la busqueda de personajes y filtro por status

--- src/components/CharacterSearch.jsx
+++ src/components/CharacterSearch.jsx
@@ -2,12 +2,86 @@ import React, { useState} from "react";
 import axios from "axios";
 
 
- 
+
+function App() {
+  const [characters, setCharacters] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [statusFilter, setStatusFilter] = useState("all");
+
+  return (
+    <div>
+      <form onSubmit={handleFormSubmit}>
+        <label htmlFor="search-term">Buscar por nombre:</label> 
+        <input
+          id="search-term"
+          type="text"
+          value={searchTerm}
+          onChange={handleSearchTermChange}
+        /> <br />
+        <br />
+        <label htmlFor="status-filter">Filtrar por estado:</label>
+        <select id="status-filter" value={statusFilter} onChange={handleStatusFilterChange}>
+          <option value="all">Todos</option>
+          <option value="alive">Vivos</option>
+          <option value="dead">Muertos</option>
+          <option value="unknown">Desconocidos</option>
+        </select>
+        <br />
+        <br />
+        <button  className="btn btn-dark" type="submit" >Buscar</button>
+      </form>
+      <br />
+
+      <ul>
+        {characters.map((character) => (
+          <li key={character.id}>
+            <br />
+            <img src={character.image} alt={character.name} />
+            <br />
+            {character.name} ({character.status})
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+  function searchCharacters() {
+    const apiUrl = "https://rickandmortyapi.com/api/character";
+    const searchParams = {};
+  
+    if (searchTerm) {
+      searchParams.name = searchTerm;
+    }
+  
+    if (statusFilter !== "all") {
+      searchParams.status = statusFilter;
+    }
+  
+    axios
+      .get(apiUrl, { params: searchParams })
+      .then((response) => setCharacters(response.data.results))
+      .catch((error) => console.log(error));
+  };
+  function handleSearchTermChange(event) {
+    setSearchTerm(event.target.value);
+  };
+  
+  function handleStatusFilterChange(event) {
+    setStatusFilter(event.target.value);
+  };
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    searchCharacters();
+  };
+};
+
+
+
 const Search = () => {
   const [name, setName] = useState("");
   const [results, setResults] = useState([]);
+ 
 
-  const handleChange = (event) => {
+const handleChange = (event) => {
     setName(event.target.value);
   };
 
@@ -17,7 +91,7 @@ const Search = () => {
       .get(`https://rickandmortyapi.com/api/character/?name=${name}`)
       .then((response) => setResults(response.data.results))
       .catch((error) => console.log(error));
-  };
+ };
 
   return (
     <div>
@@ -25,7 +99,7 @@ const Search = () => {
         <label>
           Nombre del personaje:
           <input type="text" value={name} onChange={handleChange} />
-        </label> <br/>
+       </label> <br/>
         <br/>
         <button className="btn btn-dark" type="submit">Buscar Personaje</button>
       </form>
@@ -33,59 +107,19 @@ const Search = () => {
       <ul>
         {results.map((result) => (
           <li key={result.id}>
-            <img src={result.image} alt={`${result.name}`} />
-           <h3>{result.name}</h3> 
+           <img src={result.image} alt={`${result.name}`} />
+          <h3>{result.name}</h3> 
            <p>{result.status}</p>
            </li>
         ))}
       </ul>
     </div>
-  );
-};
-
-export default Search;
-
-
-//export default function Formulario() {
-     /** estado para obtener informacion del personaje en base a su nombre */
-    // const [personaje, setPersonaje] = useState([]);
-
-    // const [nombre, setNombre] = useState("");
-
-    // const obtenerPersonajebyName = async () => { 
-       // const respuesta = await fetch('https://rickandmortyapi.com/api/character');
-       // const datos = await respuesta.json();
-       // setPersonaje(datos);
-       // console.log(datos);
-    // }
-     
-    // const handleName = (e) => {
-        //console.log(e.target.value);
-      //  setNombre(e.target.value);
-    //}
-
-   // const handleSubmit = (e) =>{
-      //  obtenerPersonajebyName();
-     //   e.preventDefault();
-
-    //}
     
-   // console.log(nombre);
+    
+ );
 
-  //return (
-   // <div className='container'>
-       // <form onSubmit={handleSubmit}>
-           // <label htmlFor="">Ingresa el nombre del personaje</label>
-           // <input type="text" placeholder='Ingresa el personaje' className='form-control' onChange={handleName}/><br></br>
-           // <button className='btn btn-dark' type='submit'>Buscar personaje</button>
-      //  </form>
-       // {
-               // personaje.map((personaje, indice) => {
-                    //return <div key={indice}>
-                           // <h2>{personaje.name}</h2>
-                       // </div>
-               // })
-              //  }
-  //  </div>
- // )
-//}
+  };
+
+export default App;
+
+
